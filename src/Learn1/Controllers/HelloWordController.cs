fe30a67c68a605d6da7ced5711f369b054546109
depalle1 <depalle1@CW2Y19KX1>
2017-01-27 07:13:06 +0100

    test

---
+++ src/Learn1/Controllers/HelloWordController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Learn1.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        // GET: /<controller>/
+        //public string Index(string p1, string p2)
+        //{
+        //    return System.Text.Encodings.Web.HtmlEncoder.Default.Encode($"Par 1:{p1} par 2:{p2}");
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ src/Learn1/Controllers/LoopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Learn1.Controllers
+{
+    public class LoopController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(int pTimes = 3)
+        {
+            ViewData["times"] = pTimes;
+            ViewData["What print"] = "Hi!!";
+            return View();
+        }
+    }
+}
